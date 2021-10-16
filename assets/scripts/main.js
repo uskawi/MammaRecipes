990005a7403cba6b54b513c5476ee057f4cca966
Jakub Essalhi <usksidi@gmail.com>
2021-10-16 01:07:32 +0000

    added eventListener to searchIcon, created hover effect on search bar

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -15,3 +15,16 @@ searchIcon.addEventListener("mouseenter", function (event) {
     }, 1000);
 }, false);
 
+// EventListener added to search icon to highlight search bar
+
+searchIcon.addEventListener("click", function () {
+    searchInput.style.border = "2px solid #5B98B7";
+    searchInput.style.boxShadow = "5px 10px #000";
+    searchInput.style.backgroundColor = "rgb(206, 201, 201)"
+     // time out after 2000ms
+    setTimeout(function () {
+        searchInput.style.border = "1.5px solid rgb(138, 136, 136)";
+        searchInput.style.boxShadow = "none";
+        searchInput.style.backgroundColor = "#fff"
+    }, 2000);
+}, false);
